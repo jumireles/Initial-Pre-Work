b058ecfb7c3ed0ffc00fe6a93b463d17fc5f622a
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:27:23 -0600

    JS Lesson 91 Learned how to access nested arrays using both bracket and dot notation.

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+//Learned how to access nested arrays using both bracket and dot notation.
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1];

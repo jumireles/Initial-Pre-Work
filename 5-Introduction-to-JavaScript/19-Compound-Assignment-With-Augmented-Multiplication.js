436f713dca7ed793c15151c85fa5c58e342de6c1
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:29:24 -0600

    JS Lesson 19 Learned that by using the (*=) operator you can multiple a variable by a number in a quicker way using variable*=(number).

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+//Learned that by using the (*=) operator you can multiple a variable by a number
+//in a quicker way using variable*=(number).
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a*=5;
+b*=3;
+c*=10;

1b35fe4ad00823b07ca977207187600e96d09ab5
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:33:38 -0600

    JS Lesson 27 Learned that you can add a string variable to another string variable by using the (+) operator.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+//Learned that you can add a string variable to another string variable by
+//using the (+) operator.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Juan";
+var myStr = "My name is " + myName + " and I am well!";

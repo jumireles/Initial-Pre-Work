1deaa49e6d59956aea4a8843d711e4ac8b80852a
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:39:58 -0600

    JS Lesson 100 Created a program that checked the inside of an array for certain values and then would return certain arguments if found or if not found. Difficult had to look at hints and parts of the solution. Need to remember object manupluation commands as well as how multi-dimensonal arrays work.

---
+++ 5-Introduction-to-JavaScript/100-Profile_Lookup.js
@@ -0,0 +1,53 @@
+//Created a program that checked the inside of an array for certain values and then would
+//return certain arguments if found or if not found. Difficult had to look at hints and
+//parts of the solution. Need to remember object manupluation commands as well as
+//how multi-dimensonal arrays work.
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+
+for(var i = 0; i < contacts.length; i++){
+  if(contacts[i].firstName === name){
+    if(contacts[i].hasOwnProperty(prop)){
+    return contacts[i][prop];
+    } else{
+    return "No such property";
+  }
+
+}}
+return "No such contact";
+
+
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");

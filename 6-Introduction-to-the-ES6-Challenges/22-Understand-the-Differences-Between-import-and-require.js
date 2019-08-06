fc611bfe8c4cc013ededcb50eae236ee4e32937b
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:16:29 -0600

    ES6 Lesson 22 Learned how to use the import function to import functions that are not standard in the browser modules.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,4 @@
+//Learned how to use the import function to import functions that are not standard in the browser modules.
+import { capitalizeString} from "string_functions"
+"use strict";
+capitalizeString("hello!");

63766b6ff67367275550e5b939064c7f90703048
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:38:36 -0600

    JS Lesson 37 Learned how to create a multi-dimensional array by making an array within the array such as mArray[[a,2],[a,2]].

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+//Learned how to create a multi-dimensional array by making an array within the array
+//such as mArray[[a,2],[a,2]].
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Pizza, 10"], ["Pineapple", 45]];

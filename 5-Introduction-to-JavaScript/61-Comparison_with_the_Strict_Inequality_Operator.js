acd92bd4b0ea46afe9eb88f4eb98d31f43f0e5fe
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:53:24 -0600

    JS Lesson 61 Learned how to use the strict inequality operator that will only retun true if the values being compared are the same number and the same type such as number and number not string and number.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+//Learned how to use the strict inequality operator that will only retun true if
+//the values being compared are the same number and the same type such as number and number not string and number.
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val!== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);

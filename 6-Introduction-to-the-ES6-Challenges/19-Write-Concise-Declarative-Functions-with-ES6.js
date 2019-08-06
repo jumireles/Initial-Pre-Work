3e3bd4f3e91fe909c536dc7b1e56870a43f1b662
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:15:08 -0600

    ES6 Lesson 19 Learned that in ES6 you can take out the word function when defining functions inside objects.  change code below this line

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+//Learned that in ES6 you can take out the word function when defining functions inside objects.
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);

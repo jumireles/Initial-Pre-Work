ef43d468714c04d314d26aa982907573be214289
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:49:16 -0600

    ES6 Lesson 9 Learned that in flexible functions you can set up a default parameters if the entered parameters are missing from the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+//Learned that in flexible functions you can set up a default parameters if the entered
+//parameters are missing from the function.
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6

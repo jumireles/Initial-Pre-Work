e8730d56ccc1491531a3d9b921bd89009db9acf9
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:46:32 -0600

    JS Lesson 48 Learned that you var can be global in scope and can be used anywhere in the javascript code. Also you can declare a varaible with a global scope without using

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,23 @@
+//Learned that you var can be global in scope and can be used anywhere in the
+//javascript code. Also you can declare a varaible with a global scope without using
+//the var command.
+// Declare your variable here
+oopsGlobal = 5;
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}

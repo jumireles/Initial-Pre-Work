6ea63955fb6492a423ba7a21f10e9db6119e0767
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:30:16 -0600

    JS Lesson 21 Learned how to declare stirngs to variables in which strings are letters.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+//Learned how to declare stirngs to variables in which strings are
+//letters.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Juan"
+var myLastName = "Mireles"

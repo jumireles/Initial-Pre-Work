4daa6fa688bdd144ca229b3725c212acb6e060ce
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:29:30 -0600

    JS Lesson 95 Learned how to iterate odd number in a for loop by intialzing at 1 but adding by 2 to get all odd number. Used console to check my answers.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+//Learned how to iterate odd number in a for loop by intialzing at 1 but adding by
+//2 to get all odd number. Used console to check my answers.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for(i=1; i < 10; i += 2){
+  myArray.push(i);
+  console.log(i);
+}
+
+// Only change code below this line.

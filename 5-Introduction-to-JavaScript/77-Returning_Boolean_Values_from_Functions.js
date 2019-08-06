889910f943a5d5b1737fbbc4070b86181c48cafc
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:20:29 -0600

    JS Lesson 77 Learned that you can use eqaulity operators to return boolean values (true or false) from functions instead of having to use many if else statements.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+//Learned that you can use eqaulity operators to return boolean values (true or false)
+//from functions instead of having to use many if else statements.
+function isLess(a, b) {
+  // Fix this code
+ return a < b;
+
+}
+
+// Change these values to test
+isLess(10, 15);

d96617be6538f40f967e7e4405fd7772fa903a53
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:44:53 -0600

    ES6 Lesson 1 Learned how to use the let function to make sure that var is not overridden.

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//Learned how to use the let function to make sure that var is not overridden.
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();

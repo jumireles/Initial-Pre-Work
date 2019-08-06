77e72e3e412efd22fe5369a806489093c2e3f0a9
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:39:39 -0600

    JS Lesson 39 Learned that in arrays the values are mutable which means that they can be changed by selecting the value in the array and then changing it.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+//Learned that in arrays the values are mutable which means that they can be changed
+//by selecting the value in the array and then changing it.
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;

973f7268ff8a7c92bb7319073c93b16521d98fc4
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:28:57 -0600

    JS Lesson 94 Learned how for loops work which is similar to while loops but inside the for loop 3 conditions are declared the initilization; condition; final expression and the for loop will excute the code inside of it until the condition is no longer true.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+//Learned how for loops work which is similar to while loops but inside the
+//for loop 3 conditions are declared the initilization; condition; final expression
+//and the for loop will excute the code inside of it until the condition is no longer true.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for(i = 1; i < 6; i++){
+    myArray.push(i);
+}
+// Only change code below this line.

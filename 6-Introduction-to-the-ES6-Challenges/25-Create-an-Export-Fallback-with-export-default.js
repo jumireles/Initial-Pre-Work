b8e56861a6c74e649b55d04b0d39a2e7b6ea9386
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:17:55 -0600

    ES6 Lesson 25 Learned how to create a fallback file for export files by using the export default command. Which creates a default export backup.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,4 @@
+//Learned how to create a fallback file for export files by using the export default command.
+//Which creates a default export backup.
+"use strict";
+export default function subtract(x,y) {return x - y;}

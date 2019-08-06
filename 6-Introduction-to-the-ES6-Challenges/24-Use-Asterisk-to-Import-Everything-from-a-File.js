09be4804c72e2e4b7080db7a18a5f9a199e08aa7
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:17:19 -0600

    ES6 Lesson 24 Learned how to import everything from a file by using the import * as function followed by the contents and the file name.

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+//Learned how to import everything from a file by using the import * as function followed by the
+//contents and the file name.
+"use strict";
+import * as str from "capitalize_strings";

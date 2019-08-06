acec8f63af9c5a3b985095b1ff2c37d72b0917f2
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:36:25 -0600

    JS Lesson 32 Used bracket notation to find a specific value of the string in this case the letter v.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//Used bracket notation to find a specific value of the string in this case the letter v.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];

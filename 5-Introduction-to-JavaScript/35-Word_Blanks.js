d7a67ea1a66137e435ac31da7fe01c37794e15b0
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:37:47 -0600

    JS Lesson 35 Used a word bank to make sentences using the (+) operator.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+//Used a word bank to make sentences using the (+) operator.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + "big" + " " + "dog" + " " + "ran" + " " + "quickly" + "." + " My " + "cat" + " " + "little" + " " + "hit" + " " + "slowly" + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "little", "hit", "slowly");

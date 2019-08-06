7976a3afbf8f2c5f7dc4918c306795dcccd578f0
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:47:36 -0600

    ES6 Lesson 6 Learned how to use shorthand to write one line function that use () and => to create a function that is not resued. also if only a return is needed no brackets are needed for the return.

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,6 @@
+//Learned how to use shorthand to write one line function that use () and => to create
+//a function that is not resued. also if only a return is needed no brackets are needed
+//for the return.
+const magic = () =>{
+  return new Date();
+};

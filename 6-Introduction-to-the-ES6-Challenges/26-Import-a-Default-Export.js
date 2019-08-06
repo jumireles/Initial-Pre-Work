e0a6b990f084fbcb091da172528cc205193a14bc
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:18:24 -0600

    ES6 Lesson 26 Learned how to import a default export by using the import function with the function being imported with the from keyword and lastly the file name. No curly brackets needed.

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+//Learned how to import a default export by using the import function with the function being imported with
+//the from keyword and lastly the file name. No curly brackets needed.
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);

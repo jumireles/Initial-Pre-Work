bea93981e2781b1b222a9476c5ceb8b524f60c38
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:22:34 -0600

    JS Lesson 81 Learned that object properties can be accessed by using either the dot or bracket notation method by using the variable name plus . plus the property that is being searched for.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+//Learned that object properties can be accessed by using either the dot or bracket notation
+//method by using the variable name plus . plus the property that is being searched for.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line

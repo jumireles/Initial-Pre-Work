da527337a275f0ed5081a1f3df6f67d431574ec4
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:30:55 -0600

    JS Lesson 22 Learned that you can use quotes inside a string by using the (\) operator which allows quotes to come out inside the string when printed.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+//Learned that you can use quotes inside a string by using the (\) operator
+//which allows quotes to come out inside the string when printed.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line

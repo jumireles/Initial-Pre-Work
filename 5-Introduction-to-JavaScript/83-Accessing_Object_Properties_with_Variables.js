7a15abf894b5b17af883db144c25649b536cbe4e
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:23:31 -0600

    JS Lesson 83 Learned that objects can hold variables that can be accessed by using bracket notations this then allows you to lookup all that is inside the object. Still a little confused.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+//Learned that objects can hold variables that can be accessed by using bracket notations
+//this then allows you to lookup all that is inside the object. Still a little confused.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;      // Change this Line
+var player = testObj[playerNumber];   // Change this Line

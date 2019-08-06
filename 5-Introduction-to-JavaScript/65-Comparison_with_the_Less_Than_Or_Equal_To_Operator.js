ab21cef5df3c6d7231f7ebec794d5e233fe6c98e
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:55:40 -0600

    JS Lesson 65 Learned how to use the (<=) less than equal to operator and how it can be used inside an if statement to check if something is true or false.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+//Learned how to use the (<=) less than equal to operator and how it can be
+//used inside an if statement to check if something is true or false.
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);

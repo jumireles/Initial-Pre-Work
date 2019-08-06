bdfc1699cb33024c8b660dbdb9410cd6a3eb7d21
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:32:02 -0600

    JS Lesson 24 /earned that you can use escape sequences inside strings that allow you to manipulate how the string is displayed such as /n will create a new line or \t will tab a new line all without having to do it the long way.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,4 @@
+//learned that you can use escape sequences inside strings that allow you to
+//manipulate how the string is displayed such as /n will create a new line or
+//\t will tab a new line all without having to do it the long way.
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line

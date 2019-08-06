d6bc8961d48617e75d054e09d255aa81d9fb1ef5
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:32:34 -0600

    JS Lesson 25 Learned how to use the (+) operator to build a single string is is composed of two different strings. Also need to remember that (+) operator does not add spaces.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+//Learned how to use the (+) operator to build a single string is is composed of two
+//different strings. Also need to remember that (+) operator does not add spaces.
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";

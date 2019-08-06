e53430590300d1941d002947c3f21626b37782b3
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:43:23 -0600

    JS Lesson 105 Learned how to use radix in a parseInt which helps convert a binary number into an integer.

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,9 @@
+//Learned how to use radix in a parseInt which helps convert a binary number into an integer.
+function convertToInteger(str) {
+
+  var convo = parseInt(str, 2);
+  return convo;
+
+}
+
+convertToInteger("10011");

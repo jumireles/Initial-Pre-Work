987152a578463c3083f6669c05ff71d8cb0d85ef
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:38:10 -0600

    JS Lesson 36 Learned how to create an array that holds 2 different types of values string and number.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+//Learned how to create an array that holds 2 different types of values string and number.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Me", 29];

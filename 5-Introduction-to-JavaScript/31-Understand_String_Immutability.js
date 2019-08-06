4f5bb57e1b6b9cf2b66f9fa5c84c7f301f6c8795
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:35:48 -0600

    JS Lesson 31 Learned that variables are immuntable and cannot be changed by using another variable but they can be replaced.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+//Learned that variables are immuntable and cannot be changed by using another
+//variable but they can be replaced.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me

452f01a8ae5418e4608aa35a5659b4588e8501e2
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:21:36 -0600

    JS Lesson 5 Learned how to initalize mulitiple variables and learned that variables are initially undefined until they are assinged a value

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+/*Learned how to initalize mulitiple variables and learned that variables are initially
+undefined until they are assinged a value*/
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";

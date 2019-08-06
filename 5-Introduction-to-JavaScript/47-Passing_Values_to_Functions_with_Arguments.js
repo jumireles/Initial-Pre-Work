ab8c1e6d90d65113c88a9d3d8436532ae3a99446
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:46:00 -0600

    JS Lesson 47 Learned that you can create a function with multiple parameters and that you can pass along arguments to those parameters when you call the function with the parameters that you wish to input such as additionProblem(2,2);

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+//Learned that you can create a function with multiple parameters and that you can
+//pass along arguments to those parameters when you call the function with the
+//parameters that you wish to input such as additionProblem(2,2);
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(num1, num2){
+  console.log(num1 + num2)
+}
+functionWithArgs(1,2);

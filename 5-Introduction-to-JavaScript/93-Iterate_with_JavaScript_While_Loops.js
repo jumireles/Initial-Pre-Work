17d1523cf11f18affa4a5f4022b901664bdc0796
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:28:24 -0600

    JS Lesson 93 Introduced to a while loop which runs a code multiple times until the condtion inside the while loop is no longer true.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+//Introduced to a while loop which runs a code multiple times until the condtion inside
+//the while loop is no longer true.
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5){
+  myArray.push(i);
+  i++;
+}

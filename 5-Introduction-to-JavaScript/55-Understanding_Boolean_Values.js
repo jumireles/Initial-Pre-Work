eb34db4bb6dbf75a089e417032f017ad80a1ed41
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:50:07 -0600

    JS Lesson 55 Learned that boolean values are either true or false and can be used as on or off switches inside code false is off and true is on.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//Learned that boolean values are either true or false and can be used as on or off
+//switches inside code false is off and true is on.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}

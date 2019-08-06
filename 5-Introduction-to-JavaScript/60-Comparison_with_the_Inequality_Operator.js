85842068ef6f93515f699ba9397849606dbd84ac
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:52:47 -0600

    JS Lesson 60 Learned how to use the != operator which is a inequality operator which tests to see if a value is not equal to what ever it is being compared to. If unequal it returns true if equal it returns false.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,13 @@
+//Learned how to use the != operator which is a inequality operator which tests to see
+//if a value is not equal to what ever it is being compared to. If unequal it returns true
+//if equal it returns false.
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);

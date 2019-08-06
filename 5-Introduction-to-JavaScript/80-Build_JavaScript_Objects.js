742c4a0e5e836b96e06dcf71a4eebbf55f5e6003
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:22:09 -0600

    JS Lesson 80 Learned how to create objects which is like creating a varaible but in this case curly brackets are used to put in strings or numbers for properties, which are then seperated by commas, can use arrays inside objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+//Learned how to create objects which is like creating a varaible but
+//in this case curly brackets are used to put in strings or numbers for properties,
+//which are then seperated by commas, can use arrays inside objects.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "dogs",
+  "legs": 5,
+  "tails": 2,
+  "friends": ["Many"]
+
+
+
+};

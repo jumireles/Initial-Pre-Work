f92a54ef8171508e074fe51ce9af7d5643700c92
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:31:36 -0600

    JS Lesson 99 Learned how to use a do while loop which ensures that the code inside the loop will run at least once even if the condition is false.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,14 @@
+//Learned how to use a do while loop which ensures that the code inside the loop will
+//run at least once even if the condition is false.
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do{
+  myArray.push(i);
+  i++;
+}while (i < 10) {
+
+}

e63b47660d1db1a3919dd7b2250a2f2a1791f05b
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:23:06 -0600

    JS Lesson 82 Learned how to use bracket notation to call properties from an object by typing the object name then using brackets and inside the brackets inside strings you write the name of the property if numbers you just write the number to use the property.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,14 @@
+//Learned how to use bracket notation to call properties from an object by
+//typing the object name then using brackets and inside the brackets inside strings
+//you write the name of the property if numbers you just write the number to use the property.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line

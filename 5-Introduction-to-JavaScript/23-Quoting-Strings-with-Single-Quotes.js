954eb8730dc53b7e191fe4cfac64dd0b720174a9
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:31:31 -0600

    JS Lesson 23 Learned that you can use single quotes to store strings in a variable but they must be used at both begining and end but this then allows you to use double quotes inside the string without having to use the front slash

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+//Learned that you can use single quotes to store strings in a variable but they
+//must be used at both begining and end but this then allows you to use double quotes
+//inside the string without having to use the front slash
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';

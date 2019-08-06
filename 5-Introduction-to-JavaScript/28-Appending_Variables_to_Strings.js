162d434e38ac42d1a8c7bffa6439acf514609b39
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:34:15 -0600

    JS Lesson 28 Learned that by using the (+=) operator I can append a string to another string variable and save coding space.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+//Learned that by using the (+=) operator I can append a string to another string
+//variable and save coding space.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "cool";
+var myStr = "Learning to code is ";
+myStr+=someAdjective;

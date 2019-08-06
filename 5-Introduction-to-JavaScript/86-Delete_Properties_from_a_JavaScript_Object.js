5533bbabcc235260114ad7e30fe2fb92ad188986
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:24:53 -0600

    JS Lesson 86 Learned how to delete an objects property by starting off with delete and then selecting the objects property just as if you were calling it.

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,24 @@
+//Learned how to delete an objects property by starting off with delete and then
+//selecting the objects property just as if you were calling it.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog.tails;

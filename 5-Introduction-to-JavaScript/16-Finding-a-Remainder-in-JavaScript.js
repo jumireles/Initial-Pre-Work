decf546dfbd15d522ccb4c8921ed67a6a6d5e761
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:27:44 -0600

    JS Lesson 16 Learned that the (%) can give you the remainder of two numbers being divded and that this helps determining if a number is even or odd. Only change code below this line

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+//Learned that the (%) can give you the remainder of two numbers being divded
+//and that this helps determining if a number is even or odd.
+// Only change code below this line
+
+var remainder = 11 % 3;

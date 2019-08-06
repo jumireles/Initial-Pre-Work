106eeaec777d371117856b2f0e5bc7b4f905e7cd
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:19:58 -0600

    JS Lesson 2 Learned how to create variables in javascript by starting with the "var" line and then writing the variable name.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,6 @@
+//Learned how to create variables in javascript by starting with the "var" line and then writing the variable name.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;

96dd1712fde22b47f9b9f643465f3276bb0359b1
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:29:57 -0600

    JS Lesson 96 Used a for loop to count backwards and put values inside an array. Used console.log to check my answers

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+//Used a for loop to count backwards and put values inside an array.
+//Used console.log to check my answers
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(i=9; i > 0; i-=2){
+  myArray.push(i);
+  console.log(i);
+}

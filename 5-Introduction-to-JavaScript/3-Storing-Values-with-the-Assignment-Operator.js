b0a15d10a6f079dfc1110ed9b91077ed2851db98
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:20:32 -0600

    JS Lesson 3 Learned how to assinging values to variables and then assinging variables ot other variables.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+//Learned how to assinging values to variables and then assinging variables ot other variables.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;

4b04e4a2ed82dba5648cd0b723d1ce78d499bfb5
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:37:25 -0600

    JS Lesson 34 Learned to find the nth to last character in a string by using bracket notation and getting the length of the string and subtracting the value being searched for.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//Learned to find the nth to last character in a string by using bracket notation
+//and getting the length of the string and subtracting the value being searched for.
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];

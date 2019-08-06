011b0f8b1bf13f6794127663397c0c5391830af2
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:36:52 -0600

    JS Lesson 33 Learned how to get the value of the last letter in a string by using the .length -1 command.

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//Learned how to get the value of the last letter in a string by using the .length -1 command.
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];

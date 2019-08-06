dd903026e98f005089c2c67de068ce212f71b582
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:40:26 -0600

    JS Lesson 101 Learned how to generate random numbers between 0 and 1 using the math.random function. Note: math.random cannot return 0 or 1.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+//Learned how to generate random numbers between 0 and 1 using the math.random function.
+//Note: math.random cannot return 0 or 1.
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}

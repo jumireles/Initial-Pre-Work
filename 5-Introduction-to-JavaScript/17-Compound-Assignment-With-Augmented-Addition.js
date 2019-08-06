58f86bfd462287b81257e0498603d41dc54bb179
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:28:15 -0600

    JS Lesson 17 Learned that you can use the (+=) operator to make in this case addition to a variable quicker by not having to use the full code of variable + x = variable

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+/*Learned that you can use the (+=) operator to make in this case addition to a variable
+quicker by not having to use the full code of variable + x = variable*/
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a+=12;
+b+=9;
+c+=7;

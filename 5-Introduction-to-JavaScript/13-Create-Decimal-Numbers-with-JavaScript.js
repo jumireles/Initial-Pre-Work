9d695b603145304cfa89f6f688c8d52d4dcc8bc8
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:26:20 -0600

    JS Lesson 13 Learned that when you use decimals they are stored as floats and how to create variables that holds decimals

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+//Learned that when you use decimals they are stored as floats and how to create
+//variables that holds decimals
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;

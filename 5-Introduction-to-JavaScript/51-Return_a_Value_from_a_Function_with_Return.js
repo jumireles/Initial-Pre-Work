42fca2e523a16b761b2451f443728fad1040a889
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:47:58 -0600

    JS Lesson 51 Learned that you can pass values to a function with its arguments which allows the entered argument to be changed and returned with return

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+//Learned that you can pass values to a function with its arguments
+//which allows the entered argument to be changed and returned with return
+//command.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num1){
+  return num1 * 5;
+}
+
+console.log(minusSeven(10));

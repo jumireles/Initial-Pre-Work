c68cdeacd5ae3c9b8cc19fbeb3aa19d7c3a32c7c
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:43:01 -0600

    JS Lesson 104 Learned how to change a string number into an integer using the parseInt function. Also if string is not a number parseInt return NaN.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,10 @@
+//Learned how to change a string number into an integer using the parseInt function.
+//Also if string is not a number parseInt return NaN.
+function convertToInteger(str) {
+
+ var convo = parseInt(str);
+ return convo;
+
+}
+
+convertToInteger("56");

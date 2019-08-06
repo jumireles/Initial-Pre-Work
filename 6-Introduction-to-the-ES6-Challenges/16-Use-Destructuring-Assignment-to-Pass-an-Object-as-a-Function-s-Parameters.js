be078e08666414789c86dc5dbe8acd45b4843050
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:53:38 -0600

    ES6 Lesson 16 showed me how to destructure an assignmnet to pass an object as function parameters. Had to remember to use the curly brackets for max,min as those are the objects elements.

---
+++ 6-Introduction-to-the-ES6-Challenges/16-Use-Destructuring-Assignment-to-Pass-an-Object-as-a-Function-s-Parameters.js
@@ -0,0 +1,23 @@
+//showed me how to destructure an assignmnet to pass an object as function parameters.
+//Had to remember to use the curly brackets for max,min as those are the objects elements.
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+  "use strict"; // do not change this line
+
+  // change code below this line
+  return function half({max, min}) {
+    // use function argument destructuring
+    return (max + min) / 2.0;
+  };
+  // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015

21e570bab6ee187fd8e674ac18a21fa79f788493
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:40:11 -0600

    JS Lesson 40 Learned that when accessing multi-deminsonal arrays with many values, you have to make sure that you start with 0 when looking at the bracket notation the first bracket tell you the array the second bracket the value inside that array.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,8 @@
+//Learned that when accessing multi-deminsonal arrays with many values, you have
+//to make sure that you start with 0 when looking at the bracket notation the
+//first bracket tell you the array the second bracket the value inside that array.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];

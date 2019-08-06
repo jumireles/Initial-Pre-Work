53c04f743e8e4332b664d0b4800bd9c5c90ee181
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 21:14:11 -0600

    ES6 Lesson 17 Learned that you can create strings using template literals. To create them you have to use '' quotes as well as the ${variable} command. I found this to be very confusing as the map command was used but i understood the main concept.

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,27 @@
+//Learned that you can create strings using template literals.
+//To create them you have to use '' quotes as well as the ${variable} command.
+//I found this to be very confusing as the map command was used but i understood the main concept.
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+
+ const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+
+
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);

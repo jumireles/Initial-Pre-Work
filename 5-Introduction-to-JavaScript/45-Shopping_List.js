bd77a91daf535e29961093c4a567660acdde13e6
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:44:47 -0600

    JS Lesson 45 Created a shopping list that was a multi-dimensoinal array in which each sublist array contained a string and a numerical value.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,3 @@
+//Created a shopping list that was a multi-dimensoinal array in which each sublist array
+//contained a string and a numerical value.
+var myList = [["Chicken",5], ["Pizza", 2], ["Soda", 8], ["Candy", 9], ["Chips", 2]];

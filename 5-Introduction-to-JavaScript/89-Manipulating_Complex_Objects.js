73dc51e50050905c9a779234b8ee001f64782f92
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:26:29 -0600

    JS Lesson 89 Learned how to change complex objects that have arrays data structures. Learned that complex objects can have multiple types of data from strings to arrays. In this exercise I learned that each object can hold complex internal objects.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,30 @@
+//Learned how to change complex objects that have arrays data structures. Learned that
+//complex objects can have multiple types of data from strings to arrays. In this exercise
+//I learned that each object can hold complex internal objects.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+
+    {"artist": "Bon Jovi",
+    "title": "Living on a Prayer",
+    "release_year": 1985,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+
+    }
+
+];

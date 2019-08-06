573f97bb6f8e8e83839040f9a04b53a1ea1d268b
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:44:26 -0600

    JS Lesson 107 Learned how to use muliple condtional tenarary's which can subistute multiple if statements.

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,7 @@
+//Learned how to use muliple condtional tenarary's which can subistute multiple if statements.
+function checkSign(num) {
+  return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+
+}
+
+checkSign(10);

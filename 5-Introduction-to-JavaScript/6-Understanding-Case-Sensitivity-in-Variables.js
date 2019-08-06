0ea09277fc20f7e20c89c5ad04a73d8c0e430a3a
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:22:24 -0600

    JS Lesson 6 Learned how to initalize mulitiple variables and learned that variables are initially undefined until they are assinged a value

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+//Learned how to properly write variable names making sure to use an intial lowercase
+//letter and then if vairable has a second word to captilize the first letter of word.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;

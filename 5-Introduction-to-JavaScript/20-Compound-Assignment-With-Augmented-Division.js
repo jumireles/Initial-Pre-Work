70b59db99eafe53baada02a190bfceed5013de02
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:29:50 -0600

    JS Lesson 20 Learned that you can divde a variable by a number quickly by using the (/=) operator.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+//Learned that you can divde a variable by a number quickly by using the (/=) operator.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a/=12;
+b/=4;
+c/=11;

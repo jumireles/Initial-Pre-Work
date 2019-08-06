b3768f255fc8c6fbf34aa8418cdb3609d68f6fa2
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:23:57 -0600

    JS Lesson 84 Learned how to update an object by using dot or bracket notation to call it and then setting the object property equal to the new value.

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+//Learned how to update an object by using dot or bracket notation to call it and then
+//setting the object property equal to the new value.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";

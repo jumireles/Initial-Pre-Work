c9508bf59afb41e7d8f09d82eab9c319bd10e989
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:39:01 -0600

    JS Lesson 38 Learned how to find values inside an array by using bracket notation on the array that holds the value I was looking for.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+//Learned how to find values inside an array by using bracket notation on the
+//array that holds the value I was looking for.
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];

ee390c4c429d997a3be629ed02f33052d0f73db1
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:53:49 -0600

    JS Lesson 62 Learned that you can use the > sign to test if a value is greater than another value. can be used in an if statement to check true or false.

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,16 @@
+//Learned that you can use the > sign to test if a value is greater than another value.
+//can be used in an if statement to check true or false.
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);

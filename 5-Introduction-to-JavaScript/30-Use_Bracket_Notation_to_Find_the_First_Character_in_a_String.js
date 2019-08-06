3dbae031a90386fed7202dc72b5b72532ff7d91a
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:35:16 -0600

    JS Lesson 30 Learned that programing you start counting at 0 and if you use bracket notations you can find the letter that is being using when you use it.

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+//Learned that programing you start counting at 0 and if you use bracket notations
+//you can find the letter that is being using when you use it.
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];

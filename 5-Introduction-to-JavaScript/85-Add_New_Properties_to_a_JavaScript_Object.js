157ec7be37c64604e0ac31a9ec0967833a315c53
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:24:29 -0600

    JS Lesson 85 Learned how to add properites to an object by using dot or bracket notations, very similar to updating properties but instead of using dot to call a proerpty the dot is used to create the new property name and then it is set equal to the new value.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+//Learned how to add properites to an object by using dot or bracket notations,
+//very similar to updating properties but instead of using dot to call a proerpty
+//the dot is used to create the new property name and then it is set equal to the new value.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "Woof";

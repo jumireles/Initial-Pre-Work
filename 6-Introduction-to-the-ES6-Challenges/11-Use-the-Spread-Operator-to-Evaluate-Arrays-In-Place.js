4b43cc343250d582da8a60f3529d441be743d605
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:50:11 -0600

    ES6 Lesson 11 Copied the contents of an array into another array by using the rest operator.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,11 @@
+//Copied the contents of an array into another array by using the rest operator.
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+
+   arr2 = [...arr1]; // change this line
+
+
+})();
+console.log(arr2);

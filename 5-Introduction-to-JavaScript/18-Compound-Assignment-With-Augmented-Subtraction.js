aae1ba23521413228d89c57b9f39f3f188928061
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:28:51 -0600

    JS Lesson 18 Learned that you can also use the (-=) operator to subract the variable by a number in a quicker way

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+//Learned that you can also use the (-=) operator to subract the variable by a number
+//in a quicker way
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a-=6;
+b-=15;
+c-=1;

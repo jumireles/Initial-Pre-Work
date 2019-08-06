9064721a72e88534f78e4e2164a1c83ebc53465d
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:45:23 -0600

    JS Lesson 46 Learned how to create a reusable function and how to print something to the console by using the console.log command, I also learned how to recall a function by calling the functions name. Also to create a function you have to start with the word function with a unqiue name.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+//Learned how to create a reusable function and how to print something to the
+//console by using the console.log command, I also learned how to recall a function
+//by calling the functions name. Also to create a function you have to start with
+//the word function with a unqiue name.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();

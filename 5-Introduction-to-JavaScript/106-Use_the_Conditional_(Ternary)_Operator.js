18664b9efdd85d6bca9f1eea05aba1becb72c4ae
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:44:04 -0600

    JS Lesson 106 Learned how to use ternary operators (?:) which can help in re-writing if statements. such as condition ? statement if true: statement if false. Also recheck instructions and look at strict equality operators.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,8 @@
+//Learned how to use ternary operators (?:) which can help in re-writing if statements.
+//such as condition ? statement if true: statement if false.
+//Also recheck instructions and look at strict equality operators.
+function checkEqual(a, b) {
+ return a == b ? true : false;
+}
+
+checkEqual(1, 2);

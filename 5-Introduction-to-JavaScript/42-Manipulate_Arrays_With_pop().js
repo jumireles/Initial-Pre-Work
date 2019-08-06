df6ad78d437d8a2746b5448aaf80ac5fbcad44d6
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 18:42:05 -0600

    JS Lesson 42 Learned that with .pop() the value at the end of an array can be removed and if needed that removed value can be stored in a variable.

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+//Learned that with .pop() the value at the end of an array can be removed and
+//if needed that removed value can be stored in a variable.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();

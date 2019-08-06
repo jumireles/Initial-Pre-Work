826db0023fc5519e4bc25810084ba7edeae14124
JuanMireles001 <jumireles2@gmail.com>
2019-08-05 19:50:35 -0600

    ES6 Lesson 12 Learned how to destruct a line of code into something smaller

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,15 @@
+//Learned how to destruct a line of code into something smaller
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const{ tomorrow: tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
